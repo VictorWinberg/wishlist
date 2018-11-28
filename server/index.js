5b828d4258e0053e955f98ccc6e41e998ba567fd
annieleonia <annie.onnered@gmail.com>
2018-11-28 21:40:51 +0100

    Add static server file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Server!"));
+app.use(express.static(path.resolve(__dirname, "..", "dist")));
+
+app.get("/api", (req, res) => res.send("Hello Server!"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "dist", "index.html"));
+});
 
 app.listen(3008, () => console.log("App is up and running!"));
